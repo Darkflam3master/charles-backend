369c67c6b9d3b2e063e0ec7cb4acbbcc072303db
Darkflam3master <x.charlie42@gmail.com>
2025-03-06 16:15:19 -0600

    add dto to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
+  signup(dto: AuthDto) {
+    return { msg: 'signed up' };
+  }
+
   login() {
     return { msg: 'logged in' };
-  }
-  signup() {
-    return { msg: 'signed up' };
   }
 }
