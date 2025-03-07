544bc0acebf6a15fbd9a162928fa2853b30c4b12
Darkflam3master <x.charlie42@gmail.com>
2025-03-07 15:03:05 -0600

    add nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule, ConfigModule],
   controllers: [],
   providers: [],
 })
