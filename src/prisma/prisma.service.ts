88de3ac31a4bb93d2a3f4bfc94622d8241b5b157
Darkflam3master <x.charlie42@gmail.com>
2025-03-07 13:02:04 -0600

    prisma service setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,6 +5,12 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    super();
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
   }
 }
