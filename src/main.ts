bef08c6a3f9528794c7c001c7e1d16a887de809f
Darkflam3master <x.charlie42@gmail.com>
2025-03-10 15:39:01 -0500

    global at guard

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AtGuard } from './common/decorators/guards';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  //requare access token for every route
+  app.useGlobalGuards(new AtGuard());
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
