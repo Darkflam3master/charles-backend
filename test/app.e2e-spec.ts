dfc18b296a24fe15db9757bd30df775051ec2a20
Darkflam3master <x.charlie42@gmail.com>
2025-03-14 14:15:05 -0500

    e2e skeleton

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,43 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+// import * as request from 'supertest';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+describe('App e2e', () => {
+  let app: INestApplication;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
+    await app.listen(3001);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Auth', () => {
+    describe('Post /auth/signup ', () => {
+      it('should return 201 when signup is successful', async () => {
+        // const signUpDto = {
+        //   userName: 'testuser',
+        //   password: 'BigStrongPassWordComplex*8',
+        //   email: 'test@example.com',
+        // };
+        // return request(app.getHttpServer())
+        //   .post('/auth/signup')
+        //   .send(signUpDto)
+        //   .expect(201)
+        //   .then((response) => {
+        //     expect(response).toHaveProperty('access_token');
+        //   });
+      });
+    });
+    it('Post /auth/login', () => {});
+    it('Post /auth/refresh', () => {});
+    it('Post /auth/logout', () => {});
   });
 });
