9414644df47350590f019c7a495152a4bb642193
Darkflam3master <x.charlie42@gmail.com>
2025-03-31 14:37:56 -0500

    clean up

--- src/common/decorators/get-current-token.decorator.ts
+++ src/common/decorators/get-current-token.decorator.ts
@@ -1,19 +1,20 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 type RequestWithTokens = {
   cookies: { access_token?: string; refresh_token?: string };
 };
 export const GetCurrentToken = createParamDecorator(
   (tokenType: 'access_token' | 'refresh_token', context: ExecutionContext) => {
-    console.error('IN GET CURREN TOKEN');
-
     const request = context.switchToHttp().getRequest<RequestWithTokens>();
-    console.log('cookie: ', request.cookies);
     const token = request.cookies[tokenType];
 
     if (!token) {
       console.error(`${tokenType} not found in request`);
-      throw new Error('Authentication failed'); // Handle missing user
+      throw new UnauthorizedException('UNAUTHORIZED');
     }
 
     return token;
