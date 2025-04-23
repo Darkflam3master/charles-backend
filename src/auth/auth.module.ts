6b54c428af085470859749a1d9b8468071748760
Darkflam3master <x.charlie42@gmail.com>
2025-04-23 13:34:12 -0500

    add PassportModule and guards to AuthModule and UserModule providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,18 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { AtStrategy, RtStrategy } from 'src/strategies';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthUtils } from './auth.utils';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [PrismaModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AtStrategy, RtStrategy, AuthUtils, String],
+  providers: [
+    AuthService,
+    AtStrategy,
+    RtStrategy,
+    AuthUtils,
+    String,
+    PassportModule,
+  ],
 })
 export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserService } from './user.service';
+import { AtGuard } from 'src/common/guards';
+import { AtStrategy } from 'src/strategies';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AtGuard, AtStrategy, PassportModule],
 })
 export class UserModule {}
