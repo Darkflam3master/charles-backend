9bebb30971b1ccb68b88ed46e7220416b87147e7
Darkflam3master <x.charlie42@gmail.com>
2025-04-23 10:31:48 -0500

    refactor UserController: rename endpoint to 'me' and update class name

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
-import { GetCurrentUserId } from 'src/common/decorators';
+import { GetCurrentUserId, Public } from 'src/common/decorators';
 
 @Controller('user')
-export class UsersController {
+export class UserController {
   constructor(private userService: UserService) {}
 
   @Public()
-  @Get('user')
+  @Get('me')
   @HttpCode(HttpStatus.OK)
   getUser(@GetCurrentUserId() id: string) {
     return this.userService.getUser(id);
