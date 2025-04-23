1fc06dd734ed93dd040bf3260fedffd34e0da04e
Darkflam3master <x.charlie42@gmail.com>
2025-04-23 13:52:12 -0500

    remove unused Public decorator from UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
-import { GetCurrentUserId, Public } from 'src/common/decorators';
+import { GetCurrentUserId } from 'src/common/decorators';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Public()
   @Get('me')
   @HttpCode(HttpStatus.OK)
   getUser(@GetCurrentUserId() id: string) {
